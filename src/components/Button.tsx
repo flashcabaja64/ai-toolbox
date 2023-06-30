fe34a1be90559af22ac5aba71a398cf6c41818d1
Hubert Yang <offspring7890@yahoo.com>
2023-06-29 23:25:45 -0700

    made button component, transition to tailwind only

---
+++ src/components/Button.tsx
@@ -0,0 +1,16 @@
+import React, { ReactElement } from 'react';
+
+type ButtonComponentProps = {
+  className?: string;
+  children?: ReactElement
+}
+
+const Button = ({ className, children, ...rest}: ButtonComponentProps) => {
+  return (
+    <button className={`${className}`}>
+      {children}
+    </button>
+  )
+}
+
+export default Button;
