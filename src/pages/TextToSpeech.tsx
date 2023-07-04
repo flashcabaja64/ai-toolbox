ec79754fe6f2ee62228bd2023005140611cb65a7
Dustin Yang <46609531+dustiny5@users.noreply.github.com>
2023-07-03 22:22:08 -0700

    Create text to speech component.

---
+++ src/pages/TextToSpeech.tsx
@@ -0,0 +1,89 @@
+import React, { useState, useEffect } from 'react';
+import Layout from '../components/Layout';
+import { HfInference } from "@huggingface/inference";
+import { Spinner, MagnifyGlass } from '../assets/svgIcons';
+import Button from '../components/Button';
+
+const inference = new HfInference(process.env.REACT_APP_HG_TOKEN);
+
+const TextToSpeech = () => {
+
+  const [audio, setAudio] = useState<string | undefined>();
+  const [loading, setLoading] = useState<boolean | undefined>(false);
+  const [text, setText] = useState<string>("");
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setAudio(undefined)
+    setLoading(true);
+    
+    inference.textToSpeech({
+      model: 'espnet/kan-bayashi_ljspeech_vits',
+      inputs: text,
+    })
+    .then(res => {
+      setAudio(URL.createObjectURL(res));
+      setLoading(false);
+    })
+    .catch((err: any) => console.log(err))
+  }
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setText(value)
+  }
+  return (
+    <Layout className=''>
+      <h1 className='text-[50px] text-center'>
+        Text to Audio
+      </h1>
+      <section className="block m-auto w-[80vw] pb-10">
+        {/* <FormControl className="min-w-xl max-w-2xl">
+          <FormLabel>Enter Text Prompt for Image Conversion</FormLabel>
+          <TextField 
+            type="text"
+            name="text-to-image"
+            value={text}
+            disabled={loading}
+            onChange={handleInputChange}
+          />
+          <Button disabled={loading} onClick={(e) => onSubmit(e)}>
+            {loading ? <Spinner/> : "Submit"}
+          </Button>
+        </FormControl> */}
+        <form onSubmit={onSubmit}>   
+          <label htmlFor="generate-speech" className="mb-2 text-sm font-medium sr-only text-white">Generate</label>
+          <div className="relative">
+            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+              <MagnifyGlass />
+            </div>
+            <input 
+              type="search" 
+              id="generate-speech" 
+              className="block disabled:bg-gray-500 disabled:cursor-not-allowed w-full p-4 pl-10 text-sm rounded-lg bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" 
+              placeholder="Generate an Audio by text" 
+              value={text}
+              onChange={handleInputChange}
+              disabled={loading} 
+            />
+            <Button 
+              disabled={loading}
+              onClick={onSubmit}
+              className='absolute right-2.5 bottom-2.5 top-2 disabled:cursor-not-allowed'
+            >
+              {!loading ? "Generate" : <Spinner />}
+            </Button>
+          </div>
+        </form>
+      </section>
+
+      {audio && (
+        <div className='flex justify-center'>
+          <audio controls src={audio} />
+        </div>
+      )}
+    </Layout>
+  )
+}
+
+export default TextToSpeech;
