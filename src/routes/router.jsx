df479fd14428fb92ad4020b0a3d7d166c51f4dae
Dustin Yang <46609531+dustiny5@users.noreply.github.com>
2023-07-03 22:21:58 -0700

    Add text to speech router

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -3,6 +3,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home";
 import MainLayout from "../components/MainLayout";
 import TextToImage from "../pages/TextToImage";
+import TextToSpeech from "../pages/TextToSpeech";
 import ErrorPage from "../pages/ErrorPage";
 
 export const router = createBrowserRouter([
@@ -18,6 +19,10 @@ export const router = createBrowserRouter([
       {
         path: "/text-to-image",
         element: <TextToImage />
+      },
+      {
+        path: "/text-to-speech",
+        element: <TextToSpeech />
       }
     ]
   },
