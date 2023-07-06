af35402f2ef4848cc6db2ac17c9f9478e6fbbe17
Dustin Yang <46609531+dustiny5@users.noreply.github.com>
2023-07-05 19:50:58 -0700

    APIs to call the different tools

---
+++ src/hooks/useInferenceAPI.ts
@@ -0,0 +1,49 @@
+import { HfInference } from "@huggingface/inference";
+import { ToolType } from '../constants/constant';
+
+const inference = new HfInference(process.env.REACT_APP_HG_TOKEN);
+
+export const useInferenceAPI = (
+  toolName: string, 
+  setBlob: React.Dispatch<React.SetStateAction<string | undefined>>, 
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>, 
+  text: string) => {
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setBlob(undefined);
+    setLoading(true);
+
+    switch(toolName) {
+      case ToolType.IMAGE:
+        try {
+          const res = await inference.textToImage({
+            model: 'stabilityai/stable-diffusion-2',
+            inputs: text,
+          })
+          setBlob(URL.createObjectURL(res));
+          setLoading(false);
+        } catch (e) {
+          console.log(e)
+        }
+        break;
+        case ToolType.SPEECH:
+          try {
+            const res = await inference.textToSpeech({
+              model: 'espnet/kan-bayashi_ljspeech_vits',
+              inputs: text,
+            })
+            setBlob(URL.createObjectURL(res));
+            setLoading(false);
+          } catch (e) {
+            console.log(e)
+          }
+          break;
+        default:
+          console.log('UNCATCHED CONDITION!');
+          break;
+    }
+  }
+  return onSubmit;
+  
+}
