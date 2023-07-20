25a24d6217caff1bf630b0b28af0e19860fc17a5
Hubert Yang <offspring7890@yahoo.com>
2023-07-20 00:21:08 -0700

    header title filtering

--- src/assets/svgIcons.tsx
+++ src/assets/svgIcons.tsx
@@ -121,3 +121,11 @@ export const CloseButton = ({className, close }: DefaultSVGProps) => {
     </svg>
   )
 }
+
+export const SendIcon = () => {
+  return (
+    <svg className="w-4 h-4 transform rotate-45 -mt-px" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"></path>
+    </svg>
+  )
+}
---
+++ src/components/Chat/ChatHeader.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { MagnifyGlass, CloseButton } from '../../assets/svgIcons';
+
+type ChatHeaderProps = {
+  searchText: string;
+  setSearchText: (e: any) => void;
+  clearInput: () => void;
+  headerName: string
+}
+
+const ChatHeader = ({ searchText, setSearchText, clearInput, headerName }: ChatHeaderProps) => {
+
+  const filterHeaderName = (name: string): any => {
+    if(name.split('-').length > 1) {
+      return name.split('-').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(" ") 
+    } else {
+      return name.charAt(0).toUpperCase() + name.slice(1);
+    }
+  }
+  
+  return (
+  <div className='flex border-b-[1px] border-b-gray-400 pb-2 justify-between'>
+    <p className='my-auto font-medium'>{filterHeaderName(headerName)}</p>
+    <div className='relative'>
+      <MagnifyGlass className='inline absolute top-[10px] left-1'/>
+      <input type="text" id="chat_search" onChange={(e) => setSearchText(e.target.value)} value={searchText} placeholder="Search" className='flex border pl-8 bg-gray-200 rounded-xl focus:outline-none focus:border-indigo-300 w-[100%] h-10'/>
+      {searchText && <CloseButton className='absolute top-[12px] right-1' close={clearInput}/>}
+    </div>
+  </div>
+  )
+}
+
+export default ChatHeader;
